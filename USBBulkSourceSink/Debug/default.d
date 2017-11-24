d48d4a3e392765f3381b9de9c4bb623037414843
Maheswari <maheswari@htic.iitm.ac.in>
2017-11-24 18:06:08 +0530

    Initial
    Added camera parameter control

---
+++ USBBulkSourceSink/Debug/default.d
@@ -0,0 +1,145 @@
+default.o: ../default.c \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3system.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3types.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyfx3_api.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcstart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcend.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3dma.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3os.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3types.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3tx.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/tx_api.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/tx_port.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3os.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3error.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usb.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usbconst.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3spi.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3lpp.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-conf.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ufm-2_14.c \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3pib.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3system.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-default.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex.c \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3uart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3gpif.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3dma.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3i2c.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3lpp.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3utils.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3socket.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3descriptor.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3gpio.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/uvc.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usbconst.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcstart.h \
+ C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcend.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/sensor.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/sensor.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/camera_ptzcontrol.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/cyfxgpif2config.h \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-descriptors.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-debug.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ep0.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-gpio.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-flash.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-i2c.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ufm-2_14.c \
+ D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-fpgaconf1.c
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3system.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3types.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyfx3_api.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcstart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcend.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3dma.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3os.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3types.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3tx.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/tx_api.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/tx_port.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3os.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3error.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usb.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usbconst.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3spi.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3lpp.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-conf.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ufm-2_14.c:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3pib.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3system.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-default.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex.c:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3uart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3gpif.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3dma.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3i2c.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3lpp.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3utils.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3socket.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3descriptor.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3gpio.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/uvc.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3usbconst.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcstart.h:
+
+C:\Program\ Files\ (x86)\Cypress\EZ-USB\ FX3\ SDK\1.3\/fw_lib/1_3_3/inc/cyu3externcend.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/sensor.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/sensor.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/camera_ptzcontrol.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/cyfxgpif2config.h:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-descriptors.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-debug.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ep0.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-gpio.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-flash.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-i2c.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-ufm-2_14.c:
+
+D:\OneDrive\PANA_SENSOR\Labview\workspace\Labview\ztex\fx3/ztex-fpgaconf1.c:
